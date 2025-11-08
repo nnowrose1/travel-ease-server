54cca72cd510f8563b9160fce1f06f8bf24fe675
nnowrose1 <nowrosenahrin7@gmail.com>
2025-11-09 00:54:01 +0600

    created the server, connected to mongoDB  and created API to get allVehicles

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fawnknm.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+   const vehiclesdb = client.db('vehiclesDB');
+    const vehiclesCollection = vehiclesdb.collection('vehicles');
+
+    // getting all the vehicles
+    app.get('/allVehicles', async(req, res) => {
+        const cursor = vehiclesCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("Travel Ease server is running")
+})
+
+app.listen(port, () => {
+    console.log(`Travel ease server is running on port: ${port}`);
+})
