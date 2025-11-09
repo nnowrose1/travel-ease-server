0a1781b0a05f886087037d63ac587cd293b1ef4b
nnowrose1 <nowrosenahrin7@gmail.com>
2025-11-09 23:13:16 +0600

    created the latest-vehicles API and API to get a particular vehicle

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
-const app = express()
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 3000;
 
-// middlewares
-app.use(cors())
-app.use(express.json())
 
+
+const serviceAccount = require("./serviceKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const verifyFirebaseToken = () => {
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fawnknm.mongodb.net/?appName=Cluster0`;
 
@@ -18,28 +31,46 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-   const vehiclesdb = client.db('vehiclesDB');
-    const vehiclesCollection = vehiclesdb.collection('vehicles');
+    const vehiclesdb = client.db("vehiclesDB");
+    const vehiclesCollection = vehiclesdb.collection("vehicles");
 
     // getting all the vehicles
-    app.get('/allVehicles', async(req, res) => {
-        const cursor = vehiclesCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/allVehicles", async (req, res) => {
+      const cursor = vehiclesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // getting a particular vehicle
+    app.get("/allVehicles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await vehiclesCollection.findOne(query);
+      res.send(result);
+      if(!result) {
+        res.send({message: "Vehicle not found!"})
+      }
+    });
 
+    // getting the latest vehicles
+    app.get("/latestVehicles", async (req, res) => {
+      const cursor = vehiclesCollection.find().sort({ createdAt: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -47,11 +78,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send("Travel Ease server is running")
-})
+app.get("/", (req, res) => {
+  res.send("Travel Ease server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Travel ease server is running on port: ${port}`);
-})
+  console.log(`Travel ease server is running on port: ${port}`);
+});
